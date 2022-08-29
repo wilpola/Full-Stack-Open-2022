7b6caef2996c13b8b433212c0f79a0d28cabe1f6
wilpola <wilpolaproduction@gmail.com>
2022-08-29 17:24:36 +0300

    Initializes components

---
+++ part1/courseinfo/src/components/Content.js
@@ -0,0 +1,13 @@
+// This file contains the Content component
+// The purpose of this component is to contain all the course parts
+
+
+export const Content = () => {
+    return (
+        <>
+        <Part />
+        <Part />
+        <Part />
+        </>
+    )
+}
---
+++ part1/courseinfo/src/components/index.js
@@ -0,0 +1,8 @@
+// This file is used for importing and exporting components
+
+import { Content } from "./Content"
+import { Part } from './Part'
+
+export {
+    Content
+}
